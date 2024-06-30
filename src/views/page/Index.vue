bd2ba4477fe9c07afff00dfc1fc7819076067830
KikkiZ <zhangyozh@foxmail.com>
2024-06-30 14:32:17 +0800

    [perf] 优化 index 页面

--- src/views/page/Index.vue
+++ src/views/page/Index.vue
@@ -1,18 +1,44 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
 import { useSettingStore } from "@/store/settingStore";
 import { refreshView } from "@/core/sidebarControl";
 
 const settingStore = useSettingStore();
 
+const greetString = ref("");
+
 onMounted(() => {
     refreshView(settingStore.show_side_bar);
+
+    const hour = new Date().getHours();
+    if (hour >= 0 && hour < 6) {
+        greetString.value = "夜深了！";
+    } else if (hour >= 6 && hour < 12) {
+        greetString.value = "早上好！";
+    } else if (hour >= 12 && hour < 18) {
+        greetString.value = "下午好！";
+    } else {
+        greetString.value = "晚上好！";
+    }
 });
 </script>
 
 <template>
-    <div class="main" id="main">hello</div>
+    <div class="main" id="main">
+        <h1 class="greet">
+            {{ greetString }}
+        </h1>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+.greet {
+    margin: 0;
+    margin-left: 2em;
+    font-size: 2em;
+    line-height: 2em;
+    text-align: start;
+    color: #353535;
+}
+</style>
