0148f84cb82b2dc9403abbd67f4514dbb34eaee5
KikkiZ <zhangyozh@foxmail.com>
2024-07-25 16:57:46 +0800

    [feat] 引入日志系统, 重构初始化代码

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,24 @@
+use std::io::{Error, Write};
 use std::sync::Mutex;
 
+use ctor::ctor;
+use flexi_logger::{
+    Age, Cleanup, Criterion, DeferredNow, FileSpec, LogSpecification, Logger, Naming, WriteMode,
+    TS_DASHES_BLANK_COLONS_DOT_BLANK,
+};
 use lazy_static::lazy_static;
+use log::{info, LevelFilter, Record};
 use rusqlite::Connection;
 
 use utils::config_utils::read_config;
+use utils::resource_utils::resource_integrity_check;
 
 pub mod entity;
 pub mod handler;
 pub mod utils;
+
+const MAX_LOG_AGE: Age = Age::Day;
+const MAX_LOG_SIZE: u64 = 10 * 1024 * 1024;
 
 lazy_static! {
     static ref CONN: Mutex<Connection> = {
@@ -16,4 +27,50 @@ lazy_static! {
         let conn = Connection::open(path).unwrap();
         Mutex::new(conn)
     };
+}
+
+// 初始化程序相关资源
+// 1. 检查资源完整性
+// 2. 初始化日志
+#[ctor]
+fn initialize() {
+    resource_integrity_check();
+
+    let logspec = LogSpecification::builder()
+        .default(LevelFilter::Error)
+        .module("rust_reader", LevelFilter::Info)
+        .build();
+    let filespec = FileSpec::default()
+        .directory(read_config().log)
+        .suffix("log");
+
+    Logger::with(logspec)
+        .log_to_file(filespec)
+        .rotate(
+            Criterion::AgeOrSize(MAX_LOG_AGE, MAX_LOG_SIZE),
+            Naming::TimestampsCustomFormat {
+                current_infix: Some("current"),
+                format: "%Y-%m-%d",
+            },
+            Cleanup::KeepLogFiles(3),
+        )
+        .write_mode(WriteMode::BufferAndFlush)
+        .format_for_files(formatter)
+        .start()
+        .unwrap();
+
+    info!("资源初始化成功");
+}
+
+fn formatter(w: &mut dyn Write, now: &mut DeferredNow, record: &Record) -> Result<(), Error> {
+    write!(
+        w,
+        "[{}][ {:5} ] {}:{}: ",
+        now.format(TS_DASHES_BLANK_COLONS_DOT_BLANK),
+        record.level(),
+        record.file().unwrap_or("<unnamed>"),
+        record.line().unwrap_or(0),
+    )?;
+
+    write!(w, "{}", &record.args())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use rust_reader::{
-    handler,
-    utils::{common_utils::set_window_shadow, resource_utils::resource_integrity_check},
-};
+use rust_reader::{handler, utils::common_utils::set_window_shadow};
 
 fn main() {
-    resource_integrity_check();
-    // println!("{:?}", rust_reader::utils::config_utils::GLOBAL_CONFIG.book);
-    // println!("{:?}", GLOBAL_CONFIG.theme);
-    // println!("{:?}", GLOBAL_CONFIG.setting);
-    
-    // TODO: 重构 bookinfo 存取方式
-
     tauri::Builder::default()
         .setup(|app| {
             if cfg!(windows) {
